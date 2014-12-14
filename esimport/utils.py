024d2f9ad8625ec88aa07dc6353452d74dec236b
thor <thorwhalen1@gmail.com>
2014-12-14 12:59:14 +0100

    Reformatted code. Included function comments as function docs (triple quotes).

--- esimport/utils.py
+++ esimport/utils.py
@@ -1,25 +1,27 @@
 import sys
 
 
-
-#
-# Opens a file and returns its contents as a string after
-# encoding to UTF-8
-#
 def retrieve_file(filename):
-	with open(filename, 'rb') as f:
-		original_contents = f.read()
+    """
+    Opens a file and returns its contents as a string after
+    encoding to UTF-8
+    :param filename:
+    :return:
+    """
+    with open(filename, 'rb') as f:
+        original_contents = f.read()
 
-	decoded_contents = original_contents.decode('utf-8-sig').encode('utf-8')
-	return decoded_contents
+    decoded_contents = original_contents.decode('utf-8-sig').encode('utf-8')
+    return decoded_contents
 
 
-
-#
-# Opens a file and returns its contents as an List of lines
-# after encoding to UTF-8
-#
 def retrieve_file_lines(filename):
-	decoded_contents = retrieve_file(filename)
+    """
+    Opens a file and returns its contents as an List of lines
+    after encoding to UTF-8
+    :param filename:
+    :return:
+    """
+    decoded_contents = retrieve_file(filename)
 
-	return decoded_contents.splitlines()
+    return decoded_contents.splitlines()
